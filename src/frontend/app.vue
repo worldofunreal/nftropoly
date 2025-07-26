07e6fcc1a76e8b3ca40a429eb61546d44a850603
Habib Anwash <tesakun6464@gmail.com>
2025-07-27 06:20:05 +0700

    add android drawer

--- src/frontend/app.vue
+++ src/frontend/app.vue
@@ -11,9 +11,9 @@
       />
       
       <!-- Main Content Wrapper -->
-      <div class="flex-1 flex flex-col min-h-screen md:ml-16">
+      <div class="w-0 flex-1 flex flex-col min-h-screen md:ml-16">
         <Header @toggle-mobile-sidebar="mobileSidebarOpen = !mobileSidebarOpen" />
-        <main class="flex-1 flex flex-col">
+        <main class="flex-1">
           <NuxtPage />
         </main>
         <AppFooter />
