be0def05775c1bd66efe92b9a5c969860d177026
Habib Anwash <tesakun6464@gmail.com>
2025-07-27 08:07:55 +0700

    remove display flex as it doesnt seem to be necessary

--- src/frontend/components/SidebarNav.vue
+++ src/frontend/components/SidebarNav.vue
@@ -1,6 +1,6 @@
 <template>
   <aside
-    class="hidden md:block z-100 fixed top-0 left-0 h-screen flex flex-col bg-white/90 dark:bg-neutral-950/90 border-r border-gray-200 dark:border-gray-800 overflow-hidden group"
+    class="hidden md:block z-100 fixed top-0 left-0 h-screen bg-white/90 dark:bg-neutral-950/90 border-r border-gray-200 dark:border-gray-800 overflow-hidden group"
     :style="{ width: collapsed ? '4rem' : '13rem', transition: 'width 0.2s cubic-bezier(0.4,0,0.2,1)' }"
     @mouseenter="collapsed = false"
     @mouseleave="collapsed = true"
